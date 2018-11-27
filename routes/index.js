80a46aa2ad3607c40f2256a4f3ed95ec440d361f
Eneida <eneidarevueltas@gmail.com>
2018-11-26 22:13:16 -0800

    adds logging to routing

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,11 @@ router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+	console.log(
+		"using routes/index.js fallback for routing to " +
+			path.join(__dirname, "../../client/build/index.html")
+	);
+	res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 
 module.exports = router;
