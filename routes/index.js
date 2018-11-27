38bc9f380e6407724b1744aaeca62032b35146b0
Eneida <eneidarevueltas@gmail.com>
2018-11-26 22:16:24 -0800

    removes some routing

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,13 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
-// If no API routes are hit, send the React app
-router.use(function(req, res) {
-	console.log(
-		"using routes/index.js fallback for routing to " +
-			path.join(__dirname, "../../client/build/index.html")
-	);
-	res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-});
+// // If no API routes are hit, send the React app
+// router.use(function(req, res) {
+// 	console.log(
+// 		"using routes/index.js fallback for routing to " +
+// 			path.join(__dirname, "../client/build/index.html")
+// 	);
+// 	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 module.exports = router;
