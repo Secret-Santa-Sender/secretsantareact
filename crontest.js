a74f246cad05205f803244d3369eac29573aba2d
Eneida <eneidarevueltas@gmail.com>
2018-11-27 21:03:52 -0800

    removing crontest

--- crontest.js
+++ crontest.js
@@ -1,5 +1,11 @@
-var cron = require("node-cron");
+// var cron = require("node-cron");
 
-cron.schedule("*/5 * * * * *", () => {
-	console.log("running every 5 seconds");
-});
+// cron.schedule("*/5 * * * * *", () => {
+// 	console.log("running every 5 seconds");
+// });
+
+function test() {
+	console.log("hello world");
+}
+
+test();
