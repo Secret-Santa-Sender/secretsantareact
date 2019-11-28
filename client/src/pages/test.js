11cd06c99abe2178c0a0535e327af2a97d31a974
Eneida <eneidarevueltas@gmail.com>
2019-11-28 09:17:24 -0700

    profile updates

--- client/src/pages/test.js
+++ client/src/pages/test.js
@@ -16,12 +16,18 @@ class Test extends Component {
       id: null,
       teamName: null,
       teams: [],
-      redirectTo: null
+      redirectTo: null,
+      likes:null,
+      dislikes: null,
+      editVisible: false
 		}
 
 		this.handleTeamSubmit = this.handleTeamSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
-    this.handleTeamClick = this.handleTeamClick.bind(this)    
+    this.handleTeamClick = this.handleTeamClick.bind(this)
+    this.handleEditClick = this.handleEditClick.bind(this)
+    this.handleEditProfileSubmit = this.handleEditProfileSubmit.bind(this)    
+    
 		
 	}
 
@@ -41,7 +47,11 @@ class Test extends Component {
           tempTeamArr.push(res.data.teams[i])
         }
 
-        this.setState({id: res.data._id, teams: tempTeamArr});
+        this.setState(
+          { id: res.data._id, 
+           teams: tempTeamArr, 
+           likes: res.data.likes, 
+           dislikes: res.data.dislikes});
       })
       .catch(() => {});
   }
@@ -94,10 +104,62 @@ class Test extends Component {
 
     }
 
+    handleEditClick(event) {
+      this.setState({
+        editVisible: true
+      })
+
+    }
+
+    handleEditProfileSubmit(event) {
+      event.preventDefault()
+
+      API.updateParticipant(this.state.id, {likes: this.state.likes, dislikes: this.state.dislikes})
+      .then(this.setState({editVisible: false}))
+      .catch(console.error)
+
+    }
+    profile(){
+      return(    
+        <div>
+        <h2> my profile </h2>
+        <p>my likes:</p> {this.state.likes}
+
+        <p>my dislikes:</p>{this.state.dislikes}
+        <button onClick={this.handleEditClick}> edit profile </button>
+        
+        </div>)
+    }
+
+
+    editProfile(){
+      return(
+       <div>
+        <form>
+          What are your likes?:<br/>
+          <input type="text" name="likes" value={this.state.likes} onChange={this.handleChange}/><br/>
+          What do you not like so much?:
+          <input type="text" name="dislikes" value={this.state.dislikes} onChange={this.handleChange}/><br/>
+          submit
+          <input type="submit" value="Submit" onClick={this.handleEditProfileSubmit}/>
+        </form>
+        </div>
+
+        )
+    }
+
 render() {
 
   if (this.state.redirectTo) {
     return <Redirect to={'/teampage/'+this.state.redirectTo} />
+  }
+
+  let editProfile = <div></div>
+
+  if (this.state.editVisible === true){
+     editProfile = this.editProfile()
+  }else if (this.state.editVisible === false){
+    editProfile =  this.profile()
   }
 
   return(
@@ -108,7 +170,8 @@ render() {
     <input type="text" name="teamName" onChange={this.handleChange}/><br/>
     <input type="submit" value="Submit" onClick={this.handleTeamSubmit}/>
   </form>
-    <div>
+ 
+  <div>
   <h2> teams i'm on </h2>
            <ul>
           {this.state.teams.map (team =>{
@@ -119,7 +182,15 @@ render() {
           })}
           </ul> 
   </div>
+
+  <div>
+    {editProfile}
   </div>
+
+
+  </div>
+  
+ 
 
   );
   }
